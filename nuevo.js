1a1dd8174c6cbcca0a6c82550f9a229b73130423
samuelmejia <samuelmejiaz2005@gamail.com>
2023-02-03 11:09:49 -0500

    mi primer coi:mit

---
+++ nuevo.js
@@ -0,0 +1,53 @@
+
+//var resultado=pr1*pr2;
+//console.log("el resultado es:"  +  resultado);
+
+//condicionales
+
+  //let miEdad=18;
+
+ // if (miEdad>=18) {
+    // console.log("puedes ingresar");
+  //}
+  //else{
+   // console.log(" numero no valido");
+  //};//
+
+ // let contador=1;
+  //while(contador<51) {
+    //console.log(contador);
+    //contador=contador + 1;
+  //};
+  //for (let contador = 100; contador>=0; contador--) {
+    //console.log(contador);
+
+   // let contar=0;
+    //while(contar<=205){
+      //console.log(contar);
+       // contar=contar +1;
+       
+        
+    //};
+  
+   // for (let hola = 0; hola <= 10; hola++) {
+      //console.log(hola);
+      
+   // }
+
+    //function suma(a,b=0) {
+
+      //console.log(a+b);
+      
+   // }
+    //suma(2+4);
+
+    var miAuto ={
+
+       marca:"Ford" ,
+       modelo:"corolla",
+       ano:2002
+    };
+    
+    
+      
+    
